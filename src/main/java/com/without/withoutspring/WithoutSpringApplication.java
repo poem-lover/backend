24776e3c581a3acf4a43c3653b9b843bebb88912
LIMDANBI <dlaeksql08222@gmail.com>
2023-09-29 02:35:24 +0900

    initial setting

---
+++ src/main/java/com/without/withoutspring/WithoutSpringApplication.java
@@ -0,0 +1,13 @@
+package com.without.withoutspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithoutSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithoutSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/without/withoutspring/WithoutSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.without.withoutspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithoutSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
